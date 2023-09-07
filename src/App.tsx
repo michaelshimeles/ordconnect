5cd0983b0f3b23dc09a1b78ff1ca5d8b579d54ba
Michael Shimeles <michaelwasihun96@gmail.com>
2023-09-06 22:15:20 -0400

    Added signatures, need to fix xverse signing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Flex } from '@radix-ui/themes';
+import { Flex, Button } from '@radix-ui/themes';
 import React from 'react';
 import WalletManager, { GetSession } from './components/WalletManager';
+import { signature } from './components/signature';
 
 const App = () => {
 
@@ -11,6 +12,7 @@ const App = () => {
   return (
     <Flex direction="column" justify="between" align="center" height="100%" gap="5">
       <WalletManager />
+      <Button onClick={() => signature("Testing Signature", useSession)}>Sign</Button>
     </Flex>
   )
 }
---
+++ src/components/signature.ts
@@ -0,0 +1,77 @@
+import { BitcoinNetworkType, signTransaction } from "sats-connect";
+
+interface MyCustomWindow extends Window {
+  btc: {
+    request: any;
+    listen: any;
+  };
+}
+
+export const signature = async (
+  message: string,
+  session: {
+    cardinalAddress: string;
+    id: string;
+    ordinalAddress: string;
+    wallet: string;
+  } | null
+) => {
+  if (session?.wallet === "unisat") {
+    const signature = await (window as any).unisat.signMessage(
+      `${message} \n
+        Owned and Signed by ${session?.ordinalAddress}
+        `
+    );
+
+    if (signature) {
+      return signature;
+    }
+    return "Not Signed";
+  }
+
+  if (session?.wallet === "xverse") {
+    const signMessageOptions = {
+      payload: {
+        network: {
+          type: BitcoinNetworkType.Mainnet,
+        },
+        address: "ordinalsAddress",
+        message,
+      },
+      onFinish: (response: any) => {
+        // signature
+        alert(response);
+      },
+      onCancel: () => alert("Canceled"),
+    };
+
+    try {
+      const response = await signTransaction(signMessageOptions as any);
+
+      if (response as any) return response;
+    } catch (error) {
+      return {
+        message: "Not signed",
+        error,
+      };
+    }
+  }
+
+  if (session?.wallet === "hiro") {
+    const customWindow: MyCustomWindow = window as unknown as MyCustomWindow;
+
+    const response = await customWindow?.btc?.request("signMessage", {
+      message: `${message} \n
+      Owned and Signed by ${session?.ordinalAddress}
+      `,
+      paymentType: "p2tr",
+    });
+
+    if (response) {
+      return response;
+    }
+    return "Not Signed";
+  }
+
+  return;
+};
